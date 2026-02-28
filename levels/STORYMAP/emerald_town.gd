0bf62d79ab996b99066ee5384d5f0933bebcb72c
ShinySkink9185 <shinyskink9185@gmail.com>
2026-02-28 18:29:22 -0500

    Character Entrances
    Adds character entrances to the dialogue. Now we're getting somewhere...!

--- levels/STORYMAP/emerald_town.gd
+++ levels/STORYMAP/emerald_town.gd
@@ -4,9 +4,12 @@ var dialogueScene = load("res://UI/Dialogue/dialogue.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var dialogue = dialogueScene.instantiate()
-	dialogue.addDialogue("Testing 1!", true)
-	dialogue.addDialogue("Testing 2!", true)
+	var dialogue = dialogueScene.instantiate(true)
+	dialogue.backgroundShade = false
+	dialogue.addSpeaker("Tails", 0, 1, "Left", "Right")
+	dialogue.addDialogue("Testing 1!", 3)
+	dialogue.addDialogue("Testing 2!")
+	dialogue.addSpeaker("Sonic", 0, 1, "Right", "Left")
 	dialogue.addDialogue("This text is really long and boring...")
 	add_child(dialogue)
 
